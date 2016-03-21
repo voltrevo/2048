ed626d9bc4e806f79c232f6b0c60eb2903a25a5e
Andrew Morris <voltrevo@gmail.com>
2016-03-21 18:04:06 +1100

    Import scss and html

--- src/application.js
+++ src/application.js
@@ -1,3 +1,7 @@
+require('./style/main.scss');
+
+const Container = require('./html/container.html');
+
 const GameManager = require('./game_manager.js');
 const KeyboardInputManager = require('./keyboard_input_manager.js');
 const LocalStorageManager = require('./local_storage_manager.js');
@@ -5,5 +9,6 @@ const HTMLActuator = require('./html_actuator.js');
 
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(() => {
+  document.body.appendChild(Container());
   window.GameManager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
