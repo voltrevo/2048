b781cec9f378489168695ce4e9a4769ad97d2a41
Andrew Morris <voltrevo@gmail.com>
2016-04-02 12:45:17 +1100

    Improve game state seed generation... a bit

--- src/cellsToSeed.js
+++ src/cellsToSeed.js
@@ -1,11 +1,12 @@
 const sqrt2 = Math.sqrt(2);
+const sqrt3 = Math.sqrt(3);
 
 module.exports = (cells) => {
   let seed = 0;
 
   for (let x = 0; x !== 4; x++) {
     for (let y = 0; y !== 4; y++) {
-      seed += (1 + 4 * x + y) * cells[x][y];
+      seed += (sqrt3 + 4 * x + y) * cells[x][y];
     }
   }
 
